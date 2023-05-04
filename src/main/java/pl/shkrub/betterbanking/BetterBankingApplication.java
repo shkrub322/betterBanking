d4eab06ce489a0064dfbd508ef1759ad1597a162
shkrub <vlad.shkrabkov@gmail.com>
2023-05-04 23:55:28 +0200

    init

---
+++ src/main/java/pl/shkrub/betterbanking/BetterBankingApplication.java
@@ -0,0 +1,13 @@
+package pl.shkrub.betterbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterBankingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BetterBankingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/shkrub/betterbanking/BetterBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.shkrub.betterbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterBankingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
